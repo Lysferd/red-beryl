50ee7ca20568f6634f35a898337a41c462291543
Lysferd <phabio_almeida@hotmail.com>
2017-09-22 16:21:38 -0300

    Initial commit for VS version of the project.

---
+++ mbi-app/mbi/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace mbi
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
