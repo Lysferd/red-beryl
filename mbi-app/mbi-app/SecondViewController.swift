9666b7e322d407060b52e0487e90295919a659f0
Lysferd <phabio_almeida@hotmail.com>
2017-10-18 17:41:11 -0200

    new attempt on project

---
+++ mbi-app/mbi-app/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  mbi-app
+//
+//  Created by 埜原菽也 on H29/10/18.
+//  Copyright © 平成29年 M.A. Eng. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
