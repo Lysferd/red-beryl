74c39f56c37bf40602b26f0e460b655498812d90
Lysferd <phabio_almeida@hotmail.com>
2017-10-18 20:26:42 -0200

    doing layout stuff

---
+++ mbi-app/mbi-app/sources/PatientTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PatientTableViewCell.swift
+//  mbi-app
+//
+//  Created by 埜原菽也 on H29/10/18.
+//  Copyright © 平成29年 M.A. Eng. All rights reserved.
+//
+
+import UIKit
+
+class PatientTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
